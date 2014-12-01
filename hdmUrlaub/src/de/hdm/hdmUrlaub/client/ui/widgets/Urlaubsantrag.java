79de1b2984d0d436b2fe9f5509a2d782c35c0bc0
Randalinho <markusschmieder@gmx.de>
2014-12-01 21:38:53 +0100

    hello

---
+++ hdmUrlaub/src/de/hdm/hdmUrlaub/client/ui/widgets/Urlaubsantrag.java
@@ -0,0 +1,20 @@
+package de.hdm.hdmUrlaub.client.ui.widgets;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.Widget;
+
+public class Urlaubsantrag extends Composite {
+
+	private static UrlaubsantragUiBinder uiBinder = GWT
+			.create(UrlaubsantragUiBinder.class);
+
+	interface UrlaubsantragUiBinder extends UiBinder<Widget, Urlaubsantrag> {
+	}
+
+	public Urlaubsantrag() {
+		initWidget(uiBinder.createAndBindUi(this));
+	}
+
+}
