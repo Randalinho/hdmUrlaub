218d4b14b691e9e348ab1f16287f47a4179b33b2
Randalinho <markusschmieder@gmx.de>
2014-12-25 18:10:53 +0100

    DataGrid eingefügt in Status

--- hdmUrlaub/src/de/hdm/hdmUrlaub/client/ui/widgets/Status.java
+++ hdmUrlaub/src/de/hdm/hdmUrlaub/client/ui/widgets/Status.java
@@ -1,12 +1,24 @@
 package de.hdm.hdmUrlaub.client.ui.widgets;
 
+import org.gwtbootstrap3.client.ui.Pagination;
+import org.gwtbootstrap3.client.ui.gwt.DataGrid;
+
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.cellview.client.TextColumn;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Widget;
+import com.google.gwt.view.client.ListDataProvider;
 
 public class Status extends Composite {
 
+	@UiField(provided = true)
+	DataGrid dataGrid = new DataGrid(10);
+	@UiField
+	Pagination dataGridPagination;
+	
+	
 	private static StatusUiBinder uiBinder = GWT.create(StatusUiBinder.class);
 
 	interface StatusUiBinder extends UiBinder<Widget, Status> {
@@ -14,6 +26,8 @@ public class Status extends Composite {
 
 	public Status() {
 		initWidget(uiBinder.createAndBindUi(this));
+		
+		
 	}
 
 }
